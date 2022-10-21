a69dec5a1c5bf3e6d7150443b2c243bc28d67786
Radolmilita <radolmilita@gmail.com>
2022-10-21 16:36:17 +0300

    Added interfaces: ITokenRepository, IPostRepository, IPersonRepository, ICommentRepository, IUserRepository

--- Domain/Interfaces/IPersonRepository.cs
+++ Domain/Interfaces/IPersonRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Interfaces
 {
-    public interface IPersonRepository
+    public interface IPersonRepository : IRepository<Person>
     {
+        Task<IEnumerable<Person>> GetAllWithDetailsAync();
+        Task<Person> GetByIdWithDetailsAsync(int id);
     }
 }
