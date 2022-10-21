dbc39edb47485c98a56aec72bdbb57facb5353bd
Radolmilita <radolmilita@gmail.com>
2022-10-21 15:24:57 +0300

    Created IUnitOfWork & IRepository

---
+++ Domain/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Delete(T entity);
+        Task DeleteByIdAsync(int id);
+        void Update(T entity);
+
+    }
+}
---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        ICommentRepository CommentRepository { get; }
+        IPersonRepository PersonRepository { get; }
+        IPostRepository PostRepository { get; }
+        ITokenRepository TokenRepository { get; }
+        IUserRepository UserRepository { get; }
+
+        Task SaveAsync();
+    }
+}
